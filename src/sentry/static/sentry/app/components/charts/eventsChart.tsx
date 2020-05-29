ded6875bf8402acccaa3dc31763b28c710d49d94
Evan Hicks <evan.hicks@sentry.io>
2020-05-29 16:42:44 -0400

    fix(perf) Change error_rate to failure_rate
    Changing error_rate to failure_rate because error_rate can be confused with
    actual error events instead of failed transactions. Rename this now before too
    many people start using it.

--- src/sentry/static/sentry/app/components/charts/eventsChart.tsx
+++ src/sentry/static/sentry/app/components/charts/eventsChart.tsx
@@ -22,7 +22,7 @@ import {getDuration, formatPercentage} from 'app/utils/formatters';
 import EventsRequest from './eventsRequest';
 
 const DURATION_AGGREGATE_PATTERN = /^(p75|p95|p99|percentile)|transaction\.duration/;
-const PERCENTAGE_AGGREGATE_PATTERN = /^(error_rate)/;
+const PERCENTAGE_AGGREGATE_PATTERN = /^(failure_rate)/;
 
 type ChartProps = {
   loading: boolean;
